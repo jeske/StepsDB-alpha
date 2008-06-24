3ff35e94936c60ede41ac6772ef138d1ff0bc659
jeske <davidj@gmail.com>
2008-06-23 23:33:14 -0700

    add whocalls

---
+++ Bend/Util/WhoCalls.cs
@@ -0,0 +1,34 @@
+﻿
+// public code from:
+// http://www.codeproject.com/KB/dotnet/MethodName.aspx
+
+using System;
+using System.Diagnostics;
+using System.Reflection;
+
+
+namespace Bend {
+    public class WhoCalls
+    {
+        private static void WhatsMyName()
+        {
+            StackFrame stackFrame = new StackFrame();
+            MethodBase methodBase = stackFrame.GetMethod();
+            Console.WriteLine(methodBase.Name); // Displays “WhatsmyName”
+
+            WhoCalledMe();
+        }
+        // Function to display parent function
+
+        private static void WhoCalledMe()
+        {
+            StackTrace stackTrace = new StackTrace();
+            StackFrame stackFrame = stackTrace.GetFrame(1);
+            MethodBase methodBase = stackFrame.GetMethod();
+            // Displays “WhatsmyName”
+
+            Console.WriteLine(" Parent Method Name {0} ", methodBase.Name);
+        }
+
+    }
+}
