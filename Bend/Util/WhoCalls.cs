45ca3166cb8074ecf2b7222a16f5c411e03fb8fb
jeske <davidj@gmail.com>
2008-06-28 23:57:44 -0700

    move whocalls test into TODO because NoInlining is broken on x64

--- Bend/Util/WhoCalls.cs
+++ Bend/Util/WhoCalls.cs
@@ -2,23 +2,26 @@
 using System;
 using System.Diagnostics;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
+// no-inlining seems to be broken on x64
+// https://connect.microsoft.com/VisualStudio/feedback/ViewFeedback.aspx?FeedbackID=162364&wa=wsignin1.0
 
 namespace Bend {
     public class WhoCalls
     {
+        [MethodImpl(MethodImplOptions.NoInlining)]
         public static string WhatsMyName()
         {
-            StackTrace stackTrace = new StackTrace();
-            StackFrame stackFrame = stackTrace.GetFrame(1);
+            StackFrame stackFrame = new StackFrame();
             MethodBase methodBase = stackFrame.GetMethod();
-
             return methodBase.Name;
         }
+        [MethodImpl(MethodImplOptions.NoInlining)]
         public static string WhoCalledMe()
         {
             StackTrace stackTrace = new StackTrace();
-            StackFrame stackFrame = stackTrace.GetFrame(2);
+            StackFrame stackFrame = stackTrace.GetFrame(1);
             MethodBase methodBase = stackFrame.GetMethod();
             
             return methodBase.Name;
@@ -28,26 +31,33 @@ namespace Bend {
     }
 }
 
-
-
 namespace BendTests
 {
     using Bend;
     using NUnit.Framework;
 
     [TestFixture]
-    public class TestWhoCalls
+    public class ZZ_TODO_WhoCalls
     {
-
-        public string TestWhoCalledMe()
-        {
-            return WhoCalls.WhoCalledMe();
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public string TestWhatsMyName() {
+            return WhoCalls.WhatsMyName();
         }
+        //[MethodImpl(MethodImplOptions.NoInlining)]
+        //public string TestWhoCalledMe() {
+        //    return WhoCalls.WhoCalledMe();
+        //}
+
         [Test]
+        [MethodImpl(MethodImplOptions.NoInlining)]
         public void T00_WhoCalls()
         {
-            Assert.AreEqual("T00_WhoCalls", WhoCalls.WhatsMyName());
-            Assert.AreEqual("T00_WhoCalls", TestWhoCalledMe());
+            Assert.AreEqual("TestWhatsMyName", TestWhatsMyName(), "1");
+
+            //Assert.AreEqual("T00_WhoCalls", TestWhoCalledMe(), "2");
+            
+            
+            
         }
     }
 }
