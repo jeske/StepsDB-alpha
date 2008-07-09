e884f87082ee2f62963db6ba0c6d9ceee69718c7
jeske <davidj@gmail.com>
2008-07-09 10:37:56 -0700

    dont use buffered writer, as it seems to be broken

--- Bend/Segment/SortedSegment.cs
+++ Bend/Segment/SortedSegment.cs
@@ -478,8 +478,7 @@ namespace Bend
             this.enumeration = enumeration;
         }
 
-        public void writeToStream(Stream rawwriter) {
-            BufferedStream writer = new BufferedStream(rawwriter);            
+        public void writeToStream(Stream writer) {
             SortedSegmentIndex index = new SortedSegmentIndex();
             SegmentWriterAdvisor advisor = new SegmentWriterAdvisor();
             int num_blocks = 0;
