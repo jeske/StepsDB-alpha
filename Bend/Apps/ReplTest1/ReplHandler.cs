34d20e6529ac1fed14a52256bbd5e62cf78c6544
jeske <davidj@gmail.com>
2010-11-28 11:03:00 -0800

    new subproject

---
+++ Bend/Apps/ReplTest1/ReplHandler.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Bend;
+
+
+
+// our keysapce schema
+
+// _my/config/ID = <server guid>
+// _my/config/quorum_requirement = <number of servers before we advance the repl tail>
+
+// _server/<SERVER GUID>/location = host:port
+
+// _logs/<SERVER GUID>/<logid> -> [update info]
+
+// _log_status/<SERVER_GUID>/repl_tail -> the oldest <logid> that may not be replicated for this server-guid
+
+namespace Bend.ReplTest1 {
+
+
+    public class ReplHandler {
+        LayerManager db;
+        public ReplHandler(LayerManager db) {
+            this.db = db;
+        }
+    }
+
+}
---
+++ Bend/Apps/ReplTest1/_Main.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Windows.Forms;
+using System.Threading;
+
+using Bend;
+
+namespace Bend.ReplTest1 {
+    class _Main {
+
+        [STAThread]
+        static void Main(string[] args) {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var window = new DbgGUI();
+            window.SetDesktopLocation(700, 200);
+
+            Thread newThread = new Thread(_Main.do_test);            
+            newThread.Start();
+            Application.Run(window);
+        }
+
+
+        static void do_test() {
+            Console.WriteLine("ReplTest1 startup...");
+            LayerManager db = new LayerManager(InitMode.NEW_REGION,@"C:\BENDtest\repl");
+            ReplHandler repl = new ReplHandler(db);
+
+
+            Console.WriteLine("waiting for key");
+            Console.ReadKey();
+            Console.WriteLine("quitting..");
+            Environment.Exit(0);
+        }
+
+    }
+
+
+
+}
