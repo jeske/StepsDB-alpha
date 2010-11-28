b66002c35d5cc94e2210d85d8722e104a8c3d304
jeske <davidj@gmail.com>
2010-11-28 10:19:52 -0800

    test project

---
+++ Bend/Apps/LinkDebug1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using anmar;
+
+
+
+namespace ConsoleApplication1 {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
