c99f8210baa2ca56cb9aebaf67e0c4a30972fac8
tomn <tomn@pobox.com>
2010-12-06 16:30:31 -0800

    Index mail mime types and run a set of queries against the results.
    Right now you have to switch the active project between IndexFiles and SearchFiles in the project.
    
    Signed-off-by: tomn <tomn@pobox.com>

--- Misc/LuceneTest/IndexFiles/BendInboxWalk.cs
+++ Misc/LuceneTest/IndexFiles/BendInboxWalk.cs
@@ -6,14 +6,48 @@ using System.Linq;
 using System.Text;
 
 using anmar.SharpMimeTools;
+using Lucene.Net.Documents;
 
 namespace IndexFiles {
 
     public class BendInboxWalk {
+        internal static readonly System.IO.FileInfo INDEX_DIR = new System.IO.FileInfo(@"c:\EmailTest\LuceneDB");
 
         [STAThread]
         public static void Main(System.String[] args) {
-            parse_email_messages();
+            System.DateTime start = System.DateTime.Now;
+            try
+            {
+                var writer = new Lucene.Net.Index.IndexWriter(Lucene.Net.Store.FSDirectory.Open(INDEX_DIR), 
+                    new Lucene.Net.Analysis.Standard.StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_CURRENT), 
+                    true, 
+                    Lucene.Net.Index.IndexWriter.MaxFieldLength.LIMITED);
+                System.Console.Out.WriteLine("Indexing to directory '" + INDEX_DIR + "'...");
+
+                // want to clean up if indexing gets "cancelled"
+                try {
+                    parse_email_messages(writer);
+                }
+                catch (Exception e)
+                {
+                    System.Console.Out.WriteLine(" caught a " + e.GetType() + "\n with message: " + e.Message);
+                }
+
+
+                System.Console.Out.WriteLine(System.DateTime.Now.Millisecond - start.Millisecond + "ms inserting docs");
+                System.Console.Out.WriteLine("Optimizing...");
+
+                writer.Optimize();
+                writer.Close();
+                
+                System.Console.Out.WriteLine(System.DateTime.Now.Millisecond - start.Millisecond + "ms total");
+            }
+            catch (System.IO.IOException e)
+            {
+                System.Console.Out.WriteLine(" caught a " + e.GetType() + "\n with message: " + e.Message);
+            }
+
+            
         }
 
         public static string UnixReadLine(Stream stream) {
@@ -30,7 +64,8 @@ namespace IndexFiles {
             return "";
         }
 
-        public static void parse_msg(string docid, string msgtxt) {
+
+        public static void parse_msg(Lucene.Net.Index.IndexWriter writer, string docid, string msgtxt) {
             if (msgtxt.Length > 4 * 1024) {
                 msgtxt = msgtxt.Substring(0, 4 * 1024 - 1);
             }
@@ -42,7 +77,10 @@ namespace IndexFiles {
                 // sharptools                
                 SharpMessage msg = new anmar.SharpMimeTools.SharpMessage(msgtxt);
                 System.Console.WriteLine("Subject: " + msg.Subject);
-
+                var doc = new Lucene.Net.Documents.Document();
+                doc.Add(new Field("body", msg.Body, Field.Store.YES, Field.Index.ANALYZED));
+                doc.Add(new Field("docid", docid, Field.Store.YES, Field.Index.NOT_ANALYZED));
+                writer.AddDocument(doc);
                 // indexer.index_document(docid, msg.Body);
             } 
 
@@ -54,7 +92,7 @@ namespace IndexFiles {
             //}                    
         }
 
-        public static void parse_email_messages() {
+        public static void parse_email_messages(Lucene.Net.Index.IndexWriter writer) {
             string basepath = @"c:\EmailTest\Data";
 
             // http://www.csharp-examples.net/get-files-from-directory/
@@ -73,7 +111,6 @@ namespace IndexFiles {
                 // LayerManager.WriteGroup txwg = new LayerManager.WriteGroup(db);
                 // txwg.add_to_log = false;
 
-
                 while (reader.Position < reader.Length - 1) {
                     string line = UnixReadLine(reader);
                     if (line.Length > 6 && line.Substring(0, 5) == "From ") {
@@ -81,17 +118,15 @@ namespace IndexFiles {
                             string msg = String.Join("\n", lines);
                             count++;
 
-     
                             string docid = fullpath + ":" + count;
-                            parse_msg(docid, msg);
+                            parse_msg(writer, docid, msg);
                         }
                             
       
                         lines = new List<string>();
                     } else {
                         lines.Add(line);
-                    }
-                    
+                    }    
                     
                 }
                 
---
+++ Misc/LuceneTest/SearchFiles/BendSearchTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Diagnostics;
+
+using Analyzer = Lucene.Net.Analysis.Analyzer;
+using StandardAnalyzer = Lucene.Net.Analysis.Standard.StandardAnalyzer;
+using Document = Lucene.Net.Documents.Document;
+using FilterIndexReader = Lucene.Net.Index.FilterIndexReader;
+using IndexReader = Lucene.Net.Index.IndexReader;
+using QueryParser = Lucene.Net.QueryParsers.QueryParser;
+using FSDirectory = Lucene.Net.Store.FSDirectory;
+using Version = Lucene.Net.Util.Version;
+using Collector = Lucene.Net.Search.Collector;
+using IndexSearcher = Lucene.Net.Search.IndexSearcher;
+using Query = Lucene.Net.Search.Query;
+using ScoreDoc = Lucene.Net.Search.ScoreDoc;
+using Scorer = Lucene.Net.Search.Scorer;
+using Searcher = Lucene.Net.Search.Searcher;
+using TopScoreDocCollector = Lucene.Net.Search.TopScoreDocCollector;
+
+namespace SearchFiles
+{
+    class BendSearchTest
+    {
+
+        [STAThread]
+        public static void Main(System.String[] args)
+        {
+            System.String index = @"c:\EmailTest\LuceneDB";
+            IndexReader reader = IndexReader.Open(FSDirectory.Open(new System.IO.FileInfo(index)), true); // only searching, so read-only=true
+            Searcher searcher = new IndexSearcher(reader);
+
+            if (Stopwatch.IsHighResolution) {
+                System.Console.WriteLine("We have a high resolution timer with an frequency of {0} ticks/ms", Stopwatch.Frequency/1000);
+            }
+
+            searchFor(searcher, "jeske AND neotonic");
+            searchFor(searcher, "noticed AND problems");
+            searchFor(searcher, "data AND returned");
+            searchFor(searcher, "scott AND hassan");
+
+            searcher.Close();
+            reader.Close();
+            System.Console.WriteLine("done");
+        }
+
+  
+        public class AnonymousClassCollector:Collector {
+			private Scorer scorer;
+			private int docBase;
+            public ArrayList hitsarray = new ArrayList();
+            public int Count { get { return hitsarray.Count; } }
+			
+			// simply print docId and score of every matching document
+			public override void Collect(int doc) {
+                string docid = doc + docBase + "";
+                // System.Console.Out.WriteLine("doc=" + docid + " score=" + scorer.Score());
+                hitsarray.Add(doc);
+ 			}
+			
+			public override bool AcceptsDocsOutOfOrder() {
+				return true;
+			}
+			
+			public override void  SetNextReader(IndexReader reader, int docBase) {
+				this.docBase = docBase;
+			}
+			
+			public override void  SetScorer(Scorer scorer) {
+				this.scorer = scorer;
+			}
+
+ 		}
+
+        public static void searchFor(Searcher searcher, string querystr) {
+            QueryParser parser = new QueryParser("body", new StandardAnalyzer());    // could be outside this function
+            Query query = parser.Parse(querystr);
+
+            var hits = new AnonymousClassCollector();
+
+            // more accurate timer
+            var timer = new Stopwatch();
+            timer.Start();
+			searcher.Search(query, hits);
+            timer.Stop();
+
+            Console.WriteLine("search for [{0}] returned {1} hits in {2}ms )",
+                query, hits.Count, timer.ElapsedMilliseconds);
+ 
+	    }
+        
+    }
+}
