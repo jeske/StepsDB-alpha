d81fa18c31f37bdfaf2542a938fe27c0ac5b1c66
jeske <davidj@gmail.com>
2010-12-11 16:55:35 -0800

    cleaned up DocumentDatabaseStage code with regions

--- Bend/Database/StepsDocumentDB.cs
+++ Bend/Database/StepsDocumentDB.cs
@@ -23,16 +23,25 @@ namespace Bend {
         IEnumerable<BsonDocument> Find(BsonDocument query_doc);
     }
 
-
-
-
     public class DocumentDatabaseStage : IStepsDocumentDB {
+
+        #region Instance Data and Constructors
         IStepsKVDB next_stage;
         static FastUniqueIds id_gen = new FastUniqueIds();
         long pk_id = -1;
         BDSkipList<long,IndexSpec> indicies = new BDSkipList<long,IndexSpec>();
 
+        public DocumentDatabaseStage(IStepsKVDB next_stage) {
+            this.next_stage = next_stage;
 
+            indicies[0] = new IndexSpec(new List<string> { "_id" }, is_primary: true);
+            pk_id = 0;
+
+        }
+
+        #endregion
+
+        #region Index Maintenance
         public struct IndexSpec {
             public readonly List<string> key_parts;
             public readonly bool is_primary;
@@ -50,14 +59,122 @@ namespace Bend {
             }
             
         }
+        #endregion
 
-        public DocumentDatabaseStage(IStepsKVDB next_stage) {
-            this.next_stage = next_stage;
+        #region Internal Helper Methods
 
-            indicies[0] = new IndexSpec(new List<string> { "_id" }, is_primary: true); 
-            pk_id = 0;
-
+        private RecordKeyType _bsonLookupToRecordKeyType(BsonDocument doc, string field_name) {
+            var element = doc.GetElement(field_name);
+            switch (element.Value.BsonType) {
+                case BsonType.String:
+                    return new RecordKeyType_String(element.Value.AsString);
+                    break;
+                case BsonType.Int64:
+                    return new RecordKeyType_Long(element.Value.AsInt64);
+                    break;
+                case BsonType.Int32:
+                    return new RecordKeyType_Long(element.Value.AsInt32);
+                    break;
+            }
+            throw new Exception("unsupported index type");
         }
+
+        private RecordKey _appendKeypartsForIndex(BsonDocument doc, IndexSpec index_spec, RecordKey index_key) {
+            foreach (var index_field_name in index_spec.key_parts) {
+                if (!doc.Contains(index_field_name)) {
+                    break; // stop building the key
+                }
+                index_key.appendKeyPart(
+                    _bsonLookupToRecordKeyType(doc, index_field_name));
+            }
+            return index_key;
+        }
+        private float _scoreIndex(BsonDocument query_doc, IndexSpec index_spec) {
+            float score = 0.0f;
+
+            // (1) We walk the prefix of each index against the query and count the 
+            // number of prefix-terms in the index match against specified parts 
+            // of the query. The longest match becomes the best index to use,
+            // and the query is executed against it.
+
+            foreach (var key_part in index_spec.key_parts) {
+                if (query_doc.Contains(key_part)) {
+                    score += 1.0f;
+                } else {
+                    break;
+                }
+            }
+            return score;
+        }
+
+        public struct ValuePair<A, B> : IComparable<ValuePair<A, B>>
+            where A : IComparable<A>
+            where B : IComparable<B> {
+            public readonly A value1;
+            public readonly B value2;
+            public int CompareTo(ValuePair<A, B> target) {
+                int cmpval = value1.CompareTo(target.value1);
+                if (cmpval != 0) { return cmpval; }
+                return value2.CompareTo(target.value2);
+            }
+            public ValuePair(A a, B b) {
+                value1 = a;
+                value2 = b;
+            }
+        }
+
+
+
+
+        private ScanRange<RecordKey> _scanRangeForQueryAndIndex(BsonDocument query_doc, long index_id) {
+            var key_prefix = new RecordKey().appendKeyPart(new RecordKeyType_Long(index_id));
+
+            IndexSpec index_spec = indicies[index_id];
+
+            foreach (var index_part_name in index_spec.key_parts) {
+                if (!query_doc.Contains(index_part_name)) {
+                    break;
+                }
+                key_prefix.appendKeyPart(_bsonLookupToRecordKeyType(query_doc, index_part_name));
+            }
+
+            return new ScanRange<RecordKey>(key_prefix, RecordKey.AfterPrefix(key_prefix), null);
+        }
+
+        private KeyValuePair<RecordKey, RecordData> _getValueHack(RecordKey key) {
+            foreach (var rec in next_stage.scanForward(
+                new ScanRange<RecordKey>(key, new ScanRange<RecordKey>.maxKey(), null))) {
+                if (rec.Key.CompareTo(key) == 0) {
+                    return rec;
+                }
+            }
+            throw new KeyNotFoundException(String.Format("_getValueHack lookup failed {0}", key));
+        }
+
+        private BsonDocument _unpackDoc(RecordData data) {
+            var ms = new MemoryStream(data.data);
+            var doc = BsonDocument.ReadFrom(ms);
+            return doc;
+        }
+
+        private bool _doesDocMatchQuery(BsonDocument doc, BsonDocument query_doc) {
+            foreach (var query_field in query_doc) {
+                if (!doc.Contains(query_field.Name)) {
+                    return false;
+                }
+                var doc_field = doc.GetElement(query_field.Name);
+                if (!doc_field.Value.Equals(query_field.Value)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        #endregion
+
+        // ---------------------------------------------------------------------
+
+        #region Public Interface Methods
 
         public void ensureIndex(string[] keys_to_index) {
             List<string> index_spec = new List<string>();
@@ -71,38 +188,13 @@ namespace Bend {
             this.indicies.Add(index_id, new IndexSpec(index_spec));
         }
 
-        private RecordKeyType _bsonLookupToRecordKeyType(BsonDocument doc, string field_name) {
-            var element = doc.GetElement(field_name);
-            switch (element.Value.BsonType) {
-                case BsonType.String:
-                    return new RecordKeyType_String(element.Value.AsString);
-                    break;
-                case BsonType.Int64:
-                    return new  RecordKeyType_Long(element.Value.AsInt64);
-                    break;
-                case BsonType.Int32:
-                    return new RecordKeyType_Long(element.Value.AsInt32);
-                    break;               
-            }
-            throw new Exception("unsupported index type");
-        }
 
-        private RecordKey _appendKeypartsForIndex(BsonDocument doc, IndexSpec index_spec, RecordKey index_key) {            
-            foreach (var index_field_name in index_spec.key_parts) {
-                if (!doc.Contains(index_field_name)) {
-                    break; // stop building the key
-                }
-                index_key.appendKeyPart(
-                    _bsonLookupToRecordKeyType(doc, index_field_name));
-            }
-            return index_key;
-        }
 
         public void Insert(BsonDocument doc) {
 
             // serialize the BsonDocument
             var ms = new MemoryStream();
-            doc.WriteTo(ms);  
+            doc.WriteTo(ms);
 
             // write the primary key
             IndexSpec pk_spec = indicies[this.pk_id];
@@ -129,83 +221,6 @@ namespace Bend {
             }
         }
 
-        public struct ValuePair<A,B> : IComparable<ValuePair<A,B>> 
-            where A : IComparable<A> 
-            where B : IComparable<B> {
-            public readonly A value1;
-            public readonly B value2;
-            public int CompareTo(ValuePair<A, B> target) {
-                int cmpval = value1.CompareTo(target.value1);
-                if (cmpval != 0) { return cmpval; }
-                return value2.CompareTo(target.value2);
-            }
-            public ValuePair(A a,B b) {
-                value1 = a;
-                value2 = b;
-            }
-        }
-
-        private float _scoreIndex(BsonDocument query_doc, IndexSpec index_spec) {
-            float score = 0.0f;
-
-            // (1) We walk the prefix of each index against the query and count the 
-            // number of prefix-terms in the index match against specified parts 
-            // of the query. The longest match becomes the best index to use,
-            // and the query is executed against it.
-
-            foreach (var key_part in index_spec.key_parts) {                
-                if (query_doc.Contains(key_part)) {
-                    score += 1.0f;
-                } else {
-                    break;
-                }
-            }
-            return score;
-        }
-
-        private ScanRange<RecordKey> _scanRangeForQueryAndIndex(BsonDocument query_doc, long index_id) {
-            var key_prefix = new RecordKey().appendKeyPart(new RecordKeyType_Long(index_id));
-
-            IndexSpec index_spec = indicies[index_id];
-
-            foreach (var index_part_name in index_spec.key_parts) {
-                if (!query_doc.Contains(index_part_name)) {
-                    break;
-                }
-                key_prefix.appendKeyPart(_bsonLookupToRecordKeyType(query_doc, index_part_name));                
-            }
-            
-            return new ScanRange<RecordKey>(key_prefix, RecordKey.AfterPrefix(key_prefix), null);
-        }
-
-        private KeyValuePair<RecordKey,RecordData> _getValueHack(RecordKey key) {
-            foreach (var rec in next_stage.scanForward(
-                new ScanRange<RecordKey>(key, new ScanRange<RecordKey>.maxKey(), null))) {
-                    if (rec.Key.CompareTo(key) == 0) {
-                        return rec;
-                    }
-            }
-            throw new KeyNotFoundException(String.Format("_getValueHack lookup failed {0}",key));
-        }
-
-        private BsonDocument _unpackDoc(RecordData data) {
-            var ms = new MemoryStream(data.data);
-            var doc = BsonDocument.ReadFrom(ms);
-            return doc;
-        }
-        
-        private bool _doesDocMatchQuery(BsonDocument doc, BsonDocument query_doc) {            
-            foreach (var query_field in query_doc) {
-                if (!doc.Contains(query_field.Name)) {
-                    return false;
-                }
-                var doc_field = doc.GetElement(query_field.Name);
-                if (!doc_field.Value.Equals(query_field.Value)) {
-                    return false;
-                }
-            }
-            return true;
-        }
 
         public IEnumerable<BsonDocument> Find(BsonDocument query_doc) {
             // (1) index selection, score all indicies
@@ -268,7 +283,12 @@ namespace Bend {
                 }
             }
         }
-    }
+
+
+        #endregion
+
+        
+    } // end of DocumentDatabaseStage
 }
 
 
