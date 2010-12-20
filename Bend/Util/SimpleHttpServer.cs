730bc2b0ef6dec3950b02e09ead674d191e93adb
jeske <davidj@gmail.com>
2010-12-19 22:01:30 -0800

    add a simple http server

---
+++ Bend/Util/SimpleHttpServer.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// started from some code posted to
+// http://www.123aspx.com/PostReview.aspx?res=75
+// 
+
+
+namespace Bend.Util {
+    using System;
+    using System.Collections;
+    using System.IO;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Threading;
+
+    class HttpProcessor {
+
+        private Socket s;
+        private BufferedStream bs;
+        private StreamReader sr;
+        private StreamWriter sw;
+        private String method;
+        private String url;
+        private String protocol;
+        private Hashtable hashTable;
+
+        public HttpProcessor(Socket s) {
+            this.s = s;
+            hashTable = new Hashtable();
+        }
+
+        public void process() {
+            NetworkStream ns = new NetworkStream(s, FileAccess.ReadWrite);
+            bs = new BufferedStream(ns);
+            sr = new StreamReader(bs);
+            sw = new StreamWriter(bs);
+            parseRequest();
+            readHeaders();
+            writeURL();
+            s.Shutdown(SocketShutdown.SdBoth);
+            ns.Close();
+        }
+
+        public void parseRequest() {
+            String request = sr.ReadLine();
+            string[] tokens = request.Split(new char[] { ' ' });
+            method = tokens[0];
+            url = tokens[1];
+            protocol = tokens[2];
+        }
+
+        public void readHeaders() {
+            String line;
+            while ((line = sr.ReadLine()) != null && line != "") {
+                string[] tokens = line.Split(new char[] { ':' });
+                String name = tokens[0];
+                String value = "";
+                for (int i = 1; i < tokens.Length; i++) {
+                    value += tokens[i];
+                    if (i < tokens.Length - 1) tokens[i] += ":";
+                }
+                hashTable[name] = value;
+            }
+        }
+
+        public void writeURL() {
+            try {
+                FileStream fs = new FileStream(url.Substring(1), FileMode.Open, FileAccess.Read);
+                writeSuccess();
+                BufferedStream bs2 = new BufferedStream(fs);
+                byte[] bytes = new byte[4096];
+                int read;
+                while ((read = bs2.Read(bytes, 0, bytes.Length)) != 0) {
+                    bs.Write(bytes, 0, read);
+                }
+                bs2.Close();
+            } catch (FileNotFoundException) {
+                writeFailure();
+                sw.WriteLine("File not found: " + url);
+            }
+            sw.Flush();
+        }
+
+        public void writeSuccess() {
+            sw.WriteLine("HTTP/1.0 200 OK");
+            sw.WriteLine("Connection: close");
+            sw.WriteLine();
+        }
+
+        public void writeFailure() {
+            sw.WriteLine("HTTP/1.0 404 File not found");
+            sw.WriteLine("Connection: close");
+            sw.WriteLine();
+        }
+    }
+
+    public class HttpServer {
+
+        // ============================================================
+        // Data
+
+        protected int port;
+
+        // ============================================================
+        // Constructor
+
+        public HttpServer()
+            : this(80) {
+        }
+
+        public HttpServer(int port) {
+            this.port = port;
+        }
+
+        // ============================================================
+        // Listener
+
+        public void listen() {
+            Socket listener = new Socket(0, SocketType.SockStream, ProtocolType.ProtTCP);
+            IPAddress ipaddress = new IPAddress("127.0.0.1");
+            IPEndPoint endpoint = new IPEndPoint(ipaddress, port);
+            listener.Bind(endpoint);
+            listener.Blocking = true;
+            listener.Listen(-1);
+            while (true) {
+                Socket s = listener.Accept();
+                HttpProcessor processor = new HttpProcessor(s);
+                Thread thread = new Thread(new ThreadStart(processor.process));
+                thread.Start();
+            }
+        }
+
+        // ============================================================
+        // Main
+
+        public static int Main(String[] args) {
+            HttpServer httpServer;
+            if (args.GetLength(0) > 0) {
+                httpServer = new HttpServer(args[0].ToUInt16());
+            } else {
+                httpServer = new HttpServer();
+            }
+            Thread thread = new Thread(new ThreadStart(httpServer.listen));
+            thread.Start();
+            return 0;
+        }
+    }
+
+}
---
+++ Misc/SimpleHttpServer/SimpleHttpServer.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+// simple HTTP explanation
+// http://www.jmarshall.com/easy/http/
+
+
+namespace Bend.Util {
+
+    public class HttpProcessor {
+        public TcpClient socket;        
+        public HttpServer srv;
+
+        private StreamReader inputStream;
+        public StreamWriter outputStream;
+
+        public String http_method;
+        public String http_url;
+        public String http_protocol_versionstring;
+        public Hashtable httpHeaders = new Hashtable();
+
+
+        private static int MAX_POST_SIZE = 10 * 1024 * 1024; // 10MB
+
+        public HttpProcessor(TcpClient s, HttpServer srv) {
+            this.socket = s;
+            this.srv = srv;                   
+        }
+
+        public void process() {            
+            // bs = new BufferedStream(s.GetStream());
+            inputStream = new StreamReader(socket.GetStream());
+            outputStream = new StreamWriter(new BufferedStream(socket.GetStream()));
+            try {
+                parseRequest();
+                readHeaders();
+                if (http_method.Equals("GET")) {
+                    handleGETRequest();
+                } else if (http_method.Equals("POST")) {
+                    handlePOSTRequest();
+                }
+            } catch (Exception e) {
+                Console.WriteLine("Exception: " + e.ToString());
+                writeFailure();
+            }
+            outputStream.Flush();
+            // bs.Flush(); // flush any remaining output
+            inputStream = null; outputStream = null; // bs = null;            
+            socket.Close();             
+        }
+
+        public void parseRequest() {
+            String request = inputStream.ReadLine();
+            string[] tokens = request.Split(new char[] { ' ' });
+            if (tokens.Length != 3) {
+                throw new Exception("invalid http request line");
+            }
+            http_method = tokens[0].ToUpper();
+            http_url = tokens[1];
+            http_protocol_versionstring = tokens[2];
+
+            Console.WriteLine("starting: " + request);
+        }
+
+        public void readHeaders() {
+            Console.WriteLine("readHeaders()");
+            String line;
+            while ((line = inputStream.ReadLine()) != null) {
+                if (line.Equals("")) {
+                    Console.WriteLine("got headers");
+                    return;
+                }
+                Console.WriteLine("header: " + line);
+                string[] tokens = line.Split(new char[] { ':' });
+                String name = tokens[0];
+                String value = "";
+                for (int i = 1; i < tokens.Length; i++) {
+                    value += tokens[i];
+                    if (i < tokens.Length - 1) tokens[i] += ":";
+                }
+                httpHeaders[name] = value;
+            }
+        }
+
+        public void handleGETRequest() {
+            srv.handleGETRequest(this);
+        }
+
+        public void handlePOSTRequest() {
+            // this post data processing just reads everything into a memory stream.
+            // this is fine for smallish things, but for large stuff we should really
+            // hand an input stream to the request processor. However, the input stream 
+            // we hand him needs to let him see the "end of the stream" at this content 
+            // length, because otherwise he won't know when he's seen it all! 
+
+            Console.WriteLine("get post data start");
+            int content_len = 0;
+            MemoryStream ms = new MemoryStream();
+            if (this.httpHeaders.ContainsKey("Content-Length")) {
+                 content_len = Convert.ToInt32(this.httpHeaders["Content-Length"]);
+                 if (content_len > MAX_POST_SIZE) {
+                     throw new Exception(
+                         String.Format("POST Content-Length({0}) too big for this simple server",
+                           content_len));
+                 }
+                 byte[] buf = new byte[4096];              
+                 int to_read = content_len;
+                 while (to_read > 0) {
+                     int numread = this.inputStream.BaseStream.Read(buf, 0, Math.Min(4096, to_read));
+                     to_read -= numread;
+                     ms.Write(buf, 0, numread);
+                 }
+                 ms.Seek(0, SeekOrigin.Begin);
+            }
+            Console.WriteLine("get post data end");
+            srv.handlePOSTRequest(this, new StreamReader(ms));
+
+        }
+
+        public void writeSuccess() {
+            outputStream.Write("HTTP/1.0 200 OK\n");
+            outputStream.Write("Content-Type: text/html\n");
+            outputStream.Write("Connection: close\n");
+            outputStream.Write("\n");
+        }
+
+        public void writeFailure() {
+            outputStream.Write("HTTP/1.0 404 File not found\n");
+            outputStream.Write("Connection: close\n");
+            outputStream.Write("\n");
+        }
+    }
+
+    public abstract class HttpServer {
+
+        protected int port;
+        TcpListener listener;
+        bool is_active = true;
+       
+        public HttpServer(int port) {
+            this.port = port;
+        }
+
+        public void listen() {
+            listener = new TcpListener(port);
+            listener.Start();
+            while (is_active) {                
+                TcpClient s = listener.AcceptTcpClient();
+                HttpProcessor processor = new HttpProcessor(s, this);
+                Thread thread = new Thread(new ThreadStart(processor.process));
+                thread.Start();
+                Thread.Sleep(1);
+            }
+        }
+
+        public abstract void handleGETRequest(HttpProcessor p);
+        public abstract void handlePOSTRequest(HttpProcessor p, StreamReader inputData);
+    }
+
+    public class MyHttpServer : HttpServer {
+        public MyHttpServer(int port)
+            : base(port) {
+        }
+        public override void handleGETRequest(HttpProcessor p) {
+            Console.WriteLine("request: {0}", p.http_url);
+            p.writeSuccess();
+            p.outputStream.WriteLine("<html><body><h1>test server</h1>");
+            p.outputStream.WriteLine("Current Time: " + DateTime.Now.ToString());
+            p.outputStream.WriteLine("url : {0}", p.http_url);
+
+            p.outputStream.WriteLine("<form method=post action=/form>");
+            p.outputStream.WriteLine("<input type=text name=foo value=foovalue>");
+            p.outputStream.WriteLine("<input type=submit name=bar value=barvalue>");
+            p.outputStream.WriteLine("</form>");
+        }
+
+        public override void handlePOSTRequest(HttpProcessor p, StreamReader inputData) {
+            Console.WriteLine("POST request: {0}", p.http_url);
+            string data = inputData.ReadToEnd();
+
+            p.outputStream.WriteLine("<html><body><h1>test server</h1>");
+            p.outputStream.WriteLine("<a href=/test>return</a><p>");
+            p.outputStream.WriteLine("postbody: <pre>{0}</pre>", data);
+            
+
+        }
+    }
+
+    public class TestMain {
+        public static int Main(String[] args) {
+            HttpServer httpServer;
+            if (args.GetLength(0) > 0) {
+                httpServer = new MyHttpServer(Convert.ToInt16(args[0]));
+            } else {
+                httpServer = new MyHttpServer(8080);
+            }
+            Thread thread = new Thread(new ThreadStart(httpServer.listen));
+            thread.Start();
+            return 0;
+        }
+
+    }
+
+}
+
+
+
